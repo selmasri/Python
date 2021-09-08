fda5a949eb341aa80d1feede8708b210e380c4c3
selmasri <selmasri@gmail.com>
2021-09-08 23:50:10 +0400

    second version

--- happy_birthday.py
+++ happy_birthday.py
@@ -1,9 +1,8 @@
 # New Prhram
-
-name = input("Enter your name ")
-birth_month = input("Enter your birth month ")
-birth_day = input("enter the birth day of the month ")
-birth_year = input("enter your birth year ")
-
-print(name,", your birthday is on", birth_month, birth_day, birth_year, "!")
+def bday_message():
+    name = input("Enter your name ")
+    birth_month = input("Enter your birth month ")
+    birth_day = input("enter the birth day of the month ")
+    birth_year = input("enter your birth year ")
+    print(name,", your birthday is on", birth_month, birth_day, birth_year, "!")
 
