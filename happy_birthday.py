091750d2b66553a087f78e119a26352708c72981
selmasri <selmasri@gmail.com>
2021-09-08 23:59:29 +0400

    Third version

--- happy_birthday.py
+++ happy_birthday.py
@@ -6,3 +6,6 @@ def bday_message():
     birth_year = input("enter your birth year ")
     print(name,", your birthday is on", birth_month, birth_day, birth_year, "!")
 
+bday_message()
+bday_message()
+#bday_message()
