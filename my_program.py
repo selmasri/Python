526e8a9142ac0d66552b505b931c5232fc30d2a5
selmasri <selmasri@gmail.com>
2021-09-09 15:46:45 +0400

    Latest Version

--- my_program.py
+++ my_program.py
@@ -3,4 +3,6 @@ my_name = "Samir El-Masri"
 print ("test", my_age, "123", my_name)
 
 my_newname = input("Enter your name ")
+my_newage = input("Enter your age")
 print("My new name is ", my_newname)
+print("My new age is ", my_newage)
