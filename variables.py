be48adc1d56fff1b573395d6456d5917503f27a3
selmasri <selmasri@gmail.com>
2021-09-09 10:43:14 +0400

    First Version

---
+++ variables.py
@@ -0,0 +1,16 @@
+MONTHS_IN_YEAR = 12
+def happy_birthday():
+    name = input("Your name: ")
+    month = input("Birth month: ")
+    day = input("Birth day of the month: ")
+    year = input("Birth year: ")
+
+    print(name, ", your birthday is on",
+        month, day, ",", year)
+
+def main():
+    happy_birthday()
+    happy_birthday()
+    happy_birthday()
+
+main()
