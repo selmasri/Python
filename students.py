95642c952ce5ea25b43f157552d377b5d1308755
sxecad <93037854+sxecad@users.noreply.github.com>
2021-11-14 14:58:14 +0400

    Nov14

--- students.py
+++ students.py
@@ -1,76 +1,90 @@
-def make_student(id, name):
+class Student:
+    #mobile = 909
+    __slots__ = ['id', 'name', '__credits', '__gpa']
+    def __init__(self, id, name, credits, gpa):
+        self.id = id
+        self.name = name
+        self.__credits = credits
+        self.__gpa = gpa
+        #self.rank = gpa*100 + credits
+    
+    def get_gpa(self):
+        return self.__gpa
+
+    def get_credits(self):
+        return self.__credits
+
+    def set_gpa(self, my_gpa):
+        self.__gpa = my_gpa
+    
+    def set_credits(self, my_credits):
+        self.__credits = my_credits
+
+
+    '''
+    id = 'No ID'
+    name = 'Student'
     credits = 0
-    gpa = 0
-
-    return id, name, credits, gpa
-'''
-def add_student(id, name):
-    population = [(100200, 'Karim', 0, 0), (100300, 'Sam', 0, 0)]
-    for index in range(len(population)):
-        if population:
-'''
-def fdict():
-    a_dict = dict()
-    b_dict = {}
-    c_dict = {'ID':100300, 'name':'Samir', 'Credit':24, 'GPA':3.5}
-    c_dict['address'] = 'Marina - Dubai'
-
-    print(c_dict)
-def names():
-    names = dict()
-    names['S'] = 'Samir'
-    names['D'] = 'Diab'
-    names['E'] = 'El-Masri'
-    names['E'] = 'Elhadi'
-    print(names)
-    print(names['E'])
-
-def more_dict():
-    a_dict = {"one":1, "two":2}
-    value = a_dict["two"]
-    print(value)
-    value = a_dict["bad key"]
-
-def check_first(a_dict, key):
-    if key in a_dict:
-        #print(value)
-        value = a_dict[key]
-        print(value)
-
-def print_dict(a_dict):
-    for key in a_dict:
-        value = a_dict[key]
-        print(key, ':', value)
-    new_list = sorted(a_dict.keys())
-    print(new_list)
-
-def add_student(a_dict, id, name, credit = 0, gpa = 0):
-
+    #hard_drive = 0 # hard drive in GB 
+    #ram = 0 # size of the ram in GB
+    gpa = 0.0   #Great Point Average
+    address = 'No address'
+    phone_number = 0
+    '''
+# This function is to print all features of the objects
+#def print_student(a_student):
+#    print(a_student.id, a_student.name, a_student.credits, a_student.gpa)
 
 def main():
-    c_dict = { 'ID':100300, 'name':'Samir', 'Credits':24, 'GPA':3.5}
-    id = 100400
-    name = 'Karim'
-    add_student(c_dict, id, name)
+    student1 = Student('200100A', 'Karim', 25, 3.7)
+    student2 = Student('200100B', 'Mary', 15, 2.9)
+    student3 = Student('8788yu', 'Mohamed', 0, 0)
+    #print_student(student1)
+    
+    print("Student1's gpa = ", student1.get_gpa())
+    print("Student2's credits = ", student2.get_credits())
+    
+    student1.set_gpa(4)
+    student2.set_credits(40)
+    
+    print("Student1's gpa = ", student1.get_gpa())
+    print("Student2's credits = ", student2.get_credits())
+   
+    #student1.__gpa = 4
+    #print_student(student1)
+    #print(student1.phone)
 
-    #check_first(c_dict, 'GPA')
-    #print_dict(c_dict)
-    #more_dict()
-    #names()
-    #fdict()
-
+    #print_student(student2)
+    #print_student(student3)
+    #student3.phone = 989898
+    #print(student3.phone)
+    #print(student1.phone)
+    #student4 = Student()
     '''
-    student1 = make_student(100200, "Karim")
-    student2 =make_student(100201, "Mary")
-    student3 =make_student(100202, "Mohammad")
-    student4 =make_student(100203, "John")
-    print(student1)
-    print(student2)
-    print(student3)
-    print(student4)
-
-    population = {}
-    print(type(population))
+    print_student(student3)
+    #student1.address ='Dubai'
+    #print(student1.phone)
+    print(student1.mobile)
+    print(student2.mobile)
+    print(student3.mobile)
+    print(Student.mobile)
     '''
+    '''
+    student1 = Student()
+    student2 = Student()
+    student3 = Student()
 
+    print_student(student1)
+    print_student(student2)
+    
+    student1.id = '200100AC'
+    student1.name = 'Karim'
+    student1.credits = 25
+    student1.gpa = 3.8
+    student2.name = 'Dana'
+    student2.address = '34 Erie St., Silicon Oasis, Dubai, UAE'
+    student2.phone_number = 576986908
+    print_student(student1)
+    print_student(student2)
+    '''
 main()
